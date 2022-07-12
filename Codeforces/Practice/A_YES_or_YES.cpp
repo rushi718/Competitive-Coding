f0b111b8fff73bafa26ff3e6635fbcaa802357da
rushi718 <41866310+rushi718@users.noreply.github.com>
2022-07-12 23:15:26 +0530

    Add files via upload

---
+++ Codeforces/Practice/A_YES_or_YES.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long    
+#define fo(i,j,n) for(int i=(j);i<((int)n);++i)
+const int mod = 1e9 + 7;
+const int N = 1e3 + 5;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+#define maxv(v) *max_element(v.begin(), v.end())
+#define minv(v) *min_element(v.begin(), v.end())
+#define sortv(v) sort(v.begin(), v.end())
+
+void solve()
+{
+    string s;
+    cin>>s;
+    transform(s.begin(), s.end(), s.begin(), ::tolower);
+    //if(s=="YES" or s=="Yes" or s=="yEs" or s=="yeS" or s=="YEs" or s=="YeS" or s=="yES" or s=="yes")
+    if(s=="yes")
+    cout<<"YES"<<endl;
+    else cout<<"NO"<<endl;
+}
+int main()
+{   ll t;
+    cin>>t;
+    //t=1;
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
+}
+
+
+
+
+
+
+
+
